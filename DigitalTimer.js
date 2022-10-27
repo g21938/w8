5df26c624bf9dfcd612eefbdee657b5e5b8a675c
g21938 <113569154+g21938@users.noreply.github.com>
2022-10-27 15:42:24 +0900

    Update DigitalTimer.js

--- DigitalTimer.js
+++ DigitalTimer.js
@@ -1,16 +1,39 @@
-function set2fig(num) {
-   // 桁数が1桁だったら先頭に0を加えて2桁に調整する
-   var ret;
-   if( num < 10 ) { ret = "0" + num; }
-   else { ret = num; }
-   return ret;
-}
-function showClock2() {
-   var nowTime = new Date();
-   var nowHour = set2fig( nowTime.getHours() );
-   var nowMin  = set2fig( nowTime.getMinutes() );
-   var nowSec  = set2fig( nowTime.getSeconds() );
-   var msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
-   document.getElementById("RealtimeClockArea2").innerHTML = msg;
-}
-setInterval('showClock2()',1000);
+const clock = () => {
+  // 現在の日時・時刻の情報を取得
+  const d = new Date();
+
+  // 年を取得
+  let year = d.getFullYear();
+  // 月を取得
+  let month = d.getMonth() + 1;
+  // 日を取得
+  let date = d.getDate();
+  // 曜日を取得
+  let dayNum = d.getDay();
+  const weekday = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  let day = weekday[dayNum];
+  // 時を取得
+  let hour = d.getHours();
+  // 分を取得
+  let min = d.getMinutes();
+  // 秒を取得
+  let sec = d.getSeconds();
+
+  // 1桁の場合は0を足して2桁に
+  month = month < 10 ? "0" + month : month;
+  date = date < 10 ? "0" + date : date;
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+
+  // 日付・時刻の文字列を作成
+  let today = `${year}.${month}.${date} ${day}`;
+  let time = `${hour}:${min}:${sec}`;
+
+  // 文字列を出力
+  document.querySelector(".clock-date").innerText = today;
+  document.querySelector(".clock-time").innerText = time;
+};
+
+// 1秒ごとにclock関数を呼び出す
+setInterval(clock, 1000);
